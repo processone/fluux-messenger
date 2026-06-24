eeed0ac66396c1107c9afc4cd13c2cbfe44320a2
Mickaël Rémond <mremond@process-one.net>
2026-06-24 21:44:18 +0200

    chore(perf): remove obsolete virtualization spike + dead alignment module (#640)
    Removes a closed 5-file cluster that's dead now that message-list
    virtualization has shipped (#638).

--- apps/fluux/src/demo/spikeEntry.tsx
+++
@@ -1,5 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import { VirtualizationSpike } from './virtualizationSpike'
-
-const root = document.getElementById('root')
-if (root) createRoot(root).render(<VirtualizationSpike />)
